8ac01e45bf24c840707a6478056806a045791668
dk-chevalier <contact@chevalierrecords.com>
2023-07-06 19:09:11 +1000

    new welcome

--- src/js/controller.js
+++ src/js/controller.js
@@ -140,5 +140,6 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
   newFeature();
+  console.log('Welcome');
 };
 init();
